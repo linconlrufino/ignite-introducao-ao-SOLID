5710ba2e3f0c204435c5b3c3b4866baa3fde8b97
Linconl Rufino <linconldasilvarufino@gmail.com>
2021-09-21 21:33:24 -0300

    Should be able to create new users

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,15 +2,13 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+    user_id: string;
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
-
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
-  }
+    constructor(private usersRepository: IUsersRepository) {}
+    execute({ user_id }: IRequest): User[] {
+        // Complete aqui
+    }
 }
-
 export { ListAllUsersUseCase };
