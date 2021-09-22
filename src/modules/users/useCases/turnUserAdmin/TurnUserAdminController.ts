9e2b071fd69a40ff31db63169c25cd3aeb3b5407
Linconl Rufino <linconldasilvarufino@gmail.com>
2021-09-21 21:47:34 -0300

    Should be able to turn an user as admin

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,11 @@ class TurnUserAdminController {
     constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
     handle(request: Request, response: Response): Response {
-        // Complete aqui
+        const { id } = request.body;
+
+        const user = this.turnUserAdminUseCase.execute(id);
+
+        return response.status(201).send();
     }
 }
 
